a37fbd53b9211fac3ed968d626db502918730942
rene42valdes <160300816+rene42valdes@users.noreply.github.com>
2024-02-20 20:58:26 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,26 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if (req.session.authorization) {
+    // get the session token
+    token = req.session.authorization['accessToken'];
+    // verify the token
+    jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+            // cache the user object in the request
+            req.user = user;
+            next();
+        }
+        // the token has expired or is invalid
+        else {
+            return res.status(403).json({message: "User not authenticated"})
+        }
+    });
+}
+// there has not been a log in even in this session
+else {
+    return res.status(403).json({message: "User not logged in"})
+}
 });
  
 const PORT =5000;
